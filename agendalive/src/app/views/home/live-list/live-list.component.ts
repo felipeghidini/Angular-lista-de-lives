4a053233bda8291f29f26c448308c6327bd0a183
felipeghidini <lipeghidini18@gmail.com>
2021-03-14 09:42:59 -0300

    Projeto iniciado e feito componentes, service e models

---
+++ agendalive/src/app/views/home/live-list/live-list.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { LiveService } from 'src/app/shared/service/live.service';
+import { Live } from 'src/app/shared/model/live.model';
+
+@Component({
+  selector: 'app-live-list',
+  templateUrl: './live-list.component.html',
+  styleUrls: ['./live-list.component.css']
+})
+export class LiveListComponent implements OnInit {
+
+  livesPrevious: Live[];
+  livesNext: Live[];
+
+
+  constructor(
+    public liveService: LiveService
+  ) { }
+
+  ngOnInit(): void {
+    this.getLives();
+  }
+
+  getLives() {
+    this.liveService.getLiveWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious)
+    });
+
+
+    this.liveService.getLiveWithFlag('next').subscribe(data => {
+      this.livesNext = data.content;
+      console.log(this.livesNext)
+    });
+  }
+
+}
